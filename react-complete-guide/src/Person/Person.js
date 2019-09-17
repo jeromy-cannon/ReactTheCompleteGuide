174ddc0d47ecbfc90c0f0c4c348c4b5d7337e239
Jeromy Cannon <jeromylewiscannon@gmail.com>
2019-09-16 21:22:32 -0500

    begin assignment 1

--- react-complete-guide/src/Person/Person.js
+++ react-complete-guide/src/Person/Person.js
@@ -1,12 +1,14 @@
 import React from 'react';
+import './Person.css';
 
 const person = (props) => {
-    return (
-        <div>
-          <p>I'm {props.name} and {props.age} years old!</p>
-          <p>{props.children}</p>
-        </div>
-    );
+  return (
+    <div className="Person">
+      <p onClick={props.click}>I'm {props.name} and {props.age} years old!</p>
+      <p>{props.children}</p>
+      <input type="text" onChange={props.changed} value={props.name} />
+    </div>
+  );
 };
 
 export default person;
