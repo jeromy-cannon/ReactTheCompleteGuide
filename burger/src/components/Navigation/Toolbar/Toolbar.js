4613015b8ffc2cd95a30bd9e2b86ebf9ffac96c1
Jeromy Cannon <jeromylewiscannon@gmail.com>
2020-03-11 10:35:20 -0500

    finished reusing the backdrop

--- burger/src/components/Navigation/Toolbar/Toolbar.js
+++ burger/src/components/Navigation/Toolbar/Toolbar.js
@@ -7,9 +7,11 @@ import NavigationItems from '../NavigationItems/NavigationItems';
 const toolbar = (props) => (
     <header className={classes.Toolbar}>
         <div>MENU</div>
-        <Logo/>
-        <nav>
-        <NavigationItems/>
+        <div className={classes.Logo}>
+            <Logo height="80%" />
+        </div>
+        <nav className={classes.DesktopOnly}>
+            <NavigationItems />
         </nav>
     </header>
 );
